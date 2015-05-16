e9550b0681c31731716b4e737d8a7f2f05ec4bb5
Federica Nocera <fnocera@uchicago.edu>
2015-05-16 18:08:51 -0500

    some functions updated

--- skeleton_project.py
+++ skeleton_project.py
@@ -1,3 +1,5 @@
+''' MAIN PROJECT PIPELINE TO PUT ALL FUNCTIONS IN FOR NFP PROJECT
+'''
 
 import numpy as np
 import pandas as pd
@@ -44,47 +46,73 @@ def bar_chart(dataframe,col_title):
 	plt.rcParams.update({'font.size': 12})
 	return bar
 
+	# preprocessing of data
+def binary_transform(df, cols):
+''' Transform True/False to 1/0'''
+	df[cols] = df[cols].applymap(lambda x: 1 if x else 0)
+	return df
+
+def cat_var_to_binary_helper(df, column_name):
+''' function that can take a categorical variable and create binary variables from it. '''
+	dummies = pd.get_dummies(df[column_name], prefix=column_name)
+	df = df.join(dummies.ix[:,:])
+	df.drop(column_name, axis=1, inplace=True) 
+	return df
+
+def cat_var_to_binary(df, cols):
+''' apply cat_var_to_binary_helper() to a list of columns'''
+	for col in cols:
+		df = cat_var_to_binary_helper(df, col)
+	return df
+
+def missing_indicator(df, column_name):
+""" add a missing indicator for a feature to the dataframe, 1 if missing and 0 otherwise. """
+	nul = df[[column_name]].  isnull()
+	nul = nul.applymap(lambda x: 1 if x else 0)
+	name = column_name + "_missing"
+	df[name] = nul
+	return df
+
+def fill_nans(df, column_name, value):
+	'''fill NaNs with value'''
+	new_df = df
+	new_df[new_df[column_name].isnull()] = new_df[new_df[column_name].isnull()].fillna(value)
+	return new_df 
+
+def fill_mode(df, column_name):
+	'''fills NaNs with mode of the column for categorical and true/false'''
+	new_df = fill_nans(df, column_name,df[column_name].mode())
+	return new_df 
+
+def fill_median(df, column_name):
+	'''fills NaNs with median of the column for numerical'''
+	new_df = fill_nans(df, column_name,df[column_name].median())
+	return new_df 
+
+
+
+if __name__ == '__main__':
+
+#upload data
 input_file = "project_data6.csv"
 df_in = readcsv_funct(input_file)
-summary_stat= stats(df_in)
+#drop rows where premature values are missing
 df = df_in.dropna(subset = ['premature'])
+summary_stat= stats(df)
 #print "stats", summary_stat
-#Want to convert variables to 0/1 so that can see fraction of types in summary stats
 
-
+#saves distributions
 pd.value_counts(df.premature).plot(kind='bar')
 col_names = ["premature","MomsRE", "HSGED", "INCOME", "MARITAL","highest_educ", "educ_currently_enrolled_type"]
 for col in col_names:
 	bar_chart(df,col)
-
 NUMERICAL = ["PREPGKG", "PREPGBMI", "age_intake_years", "edd_enrollment_interval_weeks", "gest_weeks_intake"]
-
 first_graph = df[NUMERICAL]
 bin_no = 40
 first = dist(first_graph, bin_no, "dist_1.png")
-#first[0][0].set_xlim((0,6))
-#first[0][1].set_xlim((0, 8))
-#plt.savefig("dist_1.png")
+plt.savefig("dist_1.png")
 plt.show()
 
+#split data into training and test
 
-'''
-bin_no = np.linspace(0,4, 20)
-second = dist(second_graph, bin_no, "dist_2.png")
-second[0][0].set_xlim((0, 4))
-second[0][1].set_xlim((0, 4))
-second[1][0].set_xlim((0, 4))
-#plt.savefig("dist_2.png")
 
-bin_no = 50
-#third_graph[["NumberOfOpenCreditLinesAndLoans"]].plot(kind = 'bar') 
-third = dist(third_graph, bin_no, "dist_3.png")
-third[0][0].set_xlim((0, 40))
-third[0][1].set_xlim((0, 8))
-#plt.savefig("dist_3.png")
-
-#monthlyincome = df[["MonthlyIncome"]]
-#income_bins = np.linspace(0,50000,30)
-#month = dist(monthlyincome, income_bins, "monthly_income.png")
-#plt.savefig("Monthly_Income.png")
-'''
